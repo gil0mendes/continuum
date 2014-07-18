9580991c84dc32cbd81ff09f9d9b35d2d2559087
gil0mendes <gil00mendes@gmail.com>
2014-07-18 09:25:54 +0100

    Resize map

--- src/main/java/com/continuum/Configuration.java
+++ src/main/java/com/continuum/Configuration.java
@@ -8,7 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
  */
 public class Configuration {
 	// View distance in chunks
-	public static Vector3f _viewingDistanceInChunks = new Vector3f(16.0f, 1.0f, 16.0f);
+	public static Vector3f _viewingDistanceInChunks = new Vector3f(32.0f, 1.0f, 32.0f);
 
 	// Show placing box
 	public static boolean _showPlacingBox = true;
