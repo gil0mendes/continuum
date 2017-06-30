a1ccfa658e8e56629ff68e29210bc9030fb69655
Gil Mendes <gil00mendes@gmail.com>
2017-06-30 17:52:39 +0100

    Now the game works again :D

--- facades/PC/src/main/java/org/continuum/engine/Continuum.java
+++ facades/PC/src/main/java/org/continuum/engine/Continuum.java
@@ -7,5 +7,8 @@ public final class Continuum {
 
     public static void main(String[] args) {
         System.out.println("Launcher test!!!");
+
+        // TODO just a temporary work around for this working
+        org.continuum.main.Continuum.main(args);
     }
 }
